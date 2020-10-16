7e2ba957c1c032c4a537196813d0f0481c9c7755
GameOlive <studio.gameolive@gmail.com>
2020-10-16 18:41:46 +0200

    fix: added Authentication flow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const { buildURL } = require("./components/utils.js");
 const { launch } = require("./components/launcher.js");
+const { getGames, getToken } = require("./components/games.js");
 
 module.exports = {
   launchGame: (config, callback) => launch(config, callback),
   getGameURL: (config, callback) => buildURL(config, callback),
+  getGames: (config, callback) => getGames(config, callback),
+  getToken: (config, callback) => getToken(config, callback),
 };
